17b6d1f019ba3d82534132e5a3b3df8e251aa188
JWWon <jiwoonwon@JiwoonWonui-MacBook-Pro.local>
2017-06-08 06:29:57 +0900

    validation added

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ApplicationRecord
   mount_uploader :image, S3Uploader
+  validates :artist, presence: { message: "아티스트를 입력하지 않았습니다" }
+  validates :image, presence: { message: "사진을 넣지 않았습니다" }
 end
