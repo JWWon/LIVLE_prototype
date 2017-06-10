fd8803a90b97404fa439e9601836339f8e4266f9
JWWon <jiwoonwon@JiwoonWonui-MacBook-Pro.local>
2017-06-10 21:30:35 +0900

    rollback

--- app/assets/javascripts/highlighting.js
+++ app/assets/javascripts/highlighting.js
@@ -3,37 +3,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var $window = $(window);
 
     var content_height = $(".background-video").height(); //컨텐츠 높이
+    $window.scroll(function() {
+        var cur_point = $window.scrollTop() + $window.height()/2; //가운데 지점 위치
+        var pos_num = Math.floor(cur_point / content_height) + 1; //n번째 컨텐츠
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $window.on({'touchmove': function(e) {
-            var cur_point = $window.scrollTop() + $window.height()/2; //가운데 지점 위치
-            var pos_num = Math.floor(cur_point / content_height) + 1; //n번째 컨텐츠
-            console.log(pos_num);
+        // console.log("cur position : " + pos_num);
+        // console.log("window size : " + $('#list-container').height())
 
-            // console.log("cur position : " + pos_num);
-            // console.log("window size : " + $('#list-container').height())
-
-            // 블러효과 처리
-            $("#list_"+(pos_num-2)).css('background', 'rgba(0, 0, 0, 0.65)');
-            $("#list_"+(pos_num-1)).css('background', 'rgba(0, 0, 0, 0.65)');
-            $("#list_"+pos_num).css('background', 'none');
-            $("#list_"+(pos_num+1)).css('background', 'rgba(0, 0, 0, 0.65)');
-            $("#list_"+(pos_num+2)).css('background', 'rgba(0, 0, 0, 0.65)');
-        }});
-    } else {
-        $window.scroll(function() {
-            var cur_point = $window.scrollTop() + $window.height()/2; //가운데 지점 위치
-            var pos_num = Math.floor(cur_point / content_height) + 1; //n번째 컨텐츠
-
-            // console.log("cur position : " + pos_num);
-            // console.log("window size : " + $('#list-container').height())
-
-            // 블러효과 처리
-            $("#list_"+(pos_num-2)).css('background', 'rgba(0, 0, 0, 0.65)');
-            $("#list_"+(pos_num-1)).css('background', 'rgba(0, 0, 0, 0.65)');
-            $("#list_"+pos_num).css('background', 'none');
-            $("#list_"+(pos_num+1)).css('background', 'rgba(0, 0, 0, 0.65)');
-            $("#list_"+(pos_num+2)).css('background', 'rgba(0, 0, 0, 0.65)');
-        });
-    }
+        // 블러효과 처리
+        $("#list_"+(pos_num-2)).css('background', 'rgba(0, 0, 0, 0.65)');
+        $("#list_"+(pos_num-1)).css('background', 'rgba(0, 0, 0, 0.65)');
+        $("#list_"+pos_num).css('background', 'none');
+        $("#list_"+(pos_num+1)).css('background', 'rgba(0, 0, 0, 0.65)');
+        $("#list_"+(pos_num+2)).css('background', 'rgba(0, 0, 0, 0.65)');
+    });
 });
