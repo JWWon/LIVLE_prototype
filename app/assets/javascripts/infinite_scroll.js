58208a05fee442135de614b4b63d1aff9cf7ec3e
JWWon <wonjiwn@naver.com>
2017-08-02 14:21:41 +0900

    virtual pageview updated

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var isLoading = false;
     if ($('.infinite-scrolling').size() > 0) {
         $(window).on('scroll', function() {
-            var _gaq = _gaq || [];
             var more_contents_url = $('.pagination a.next_page').attr('href');
             if (!isLoading && more_contents_url && $(window).scrollTop() > $(document).height() - $(window).height() - 240) {
                 isLoading = true;
-                _gaq.push(['_setAccount', 'UA-102852425-1'],
-                    ['_trackPageview', $(location).attr("href") + more_contents_url]);
+                ga('send', { 'hitType': 'pageview', 'page': $(location).attr("href") + more_contents_url });
                 console.log("virtual pageview: " + $(location).attr("href") + more_contents_url);
                 $.getScript(more_contents_url).done(function (data,textStatus,jqxhr) {
                     isLoading = false;
@@ -17,10 +15,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
         });
     }
-
-    (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 });
